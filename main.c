ad141bc759cf2eff8049fc0e1db5a540ae55ca2d
Jacob Littler <159176120+dankpompano@users.noreply.github.com>
2024-02-13 12:56:50 -0500

    Tuesday Lab Updates
    added comments to functions, fixed some logic errors, began testing cases

--- main.c
+++ main.c
@@ -1,9 +1,69 @@
 #include <stdio.h>
-#include <new_string.h>
+#include <new_string.h> //is not working? 
 #include <string.h>
 
 int main()
 {
-    
+
+	char array[100];
+	
+	char cat[100];
+	cat = [dog];
+	 
+	
+	//our functions
+    //new_strcpy(char* destination, const char* source)
+	//new_strncpy(char* destination, const char* source, size_t n)
+   //new_strcmp(const char* string1, const char* string2)
+   //new_strncmp(const char* string1, const char* string2, size_t n)
+   //new_strcat(char* destination, const char* source)
+   //new_strncat(char* destination, const char* source, size_t n)
+   //new_strlen(const char* string)
+   //new_strchr(const char* string, int character)
+   //new_strstr(const char* haystack, const char* needle)
+   
+	new_strcpy(array, "cater");
+	printf("%c",array);
+
+	new_strncpy(array,"doggie", 3);
+	//printf("%c",array);
+
+	new_strcmp("doh","doh");
+	//printf("%c",array);
+
+	new_strncmp("dar","cat",2);
+	//printf("%c",array);
+
+	new_strcat(array,"");
+	//printf("%c",array);
+
+	new_strncat();
+	//printf("%c",array);
+
+	new_strlen("dog");
+	//printf("%c",array);
+   
+   //new_strchr();
+   //new_strstr();
+   
+   //library functions
+
+   strcpy();
+   
+   strncpy();
+   
+   strcmp();
+   
+   strncmp();
+   
+   strcat();
+   
+   strncat();
+   
+   strlen("dog");
+   
+   //strchar();
+   
+   //strrchr();
     return 0;
 }
--- new_string.c
+++ new_string.c
@@ -1,5 +1,6 @@
 #include "new_string.h"
 
+//copies the characters from the source into the destination
 char* new_strcpy(char* destination, const char* source)
 {
     int index = 0;
@@ -11,6 +12,7 @@ char* new_strcpy(char* destination, const char* source)
     return destination;
 }
 
+//copies the first n characters from the source into the destination
 char* new_strncpy(char* destination, const char* source, size_t n)
 {
     int index = 0;
@@ -29,6 +31,7 @@ char* new_strncpy(char* destination, const char* source, size_t n)
     return destination;
 }
 
+//compares two strings. if string1 comes after string2 it returns a positive value, negative if vice versa. 0 if they are equal. 
 int new_strcmp(const char* string1, const char* string2)
 {
     int index = 0;  
@@ -41,18 +44,20 @@ int new_strcmp(const char* string1, const char* string2)
     return string1[index] - string2[index];
 }
 
+//compares two strings up to the specified index represented by the n value
 int new_strncmp(const char* string1, const char* string2, size_t n)
 {
     int index = 0;  
     //string1 at index is null end, if string2 at index is null will not be equal to string1 at index
-    //or if at any point string1 at index is not equal to string2 at index
-    while((string1[index] != '\0' && string1[index] == string2[index]) || index < n)
+    //and if at any point string1 at index is not equal to string2 at index
+    while(string1[index] != '\0' && string1[index] == string2[index] && index < n)
     {
         index++;
     }
     return string1[index] - string2[index];
 }
 
+//adds the string contained in source to the end of the string of destination.
 char* new_strcat(char* destination, const char* source)
 {
     int desIndex = new_strlen(destination);
@@ -60,27 +65,31 @@ char* new_strcat(char* destination, const char* source)
     int sourceIndex = 0;
     while(source[sourceIndex] != '\0')
     {
-        destination[desIndex] = source[sourceIndex];
-        desIndex++;     
-        sourceIndex++;
+		destination[desIndex] = source[sourceIndex];
+		desIndex++;     
+		sourceIndex++;
     }
+    destination[desIndex] = '\0';
     return destination;
 }
 
+//adds the string contained in source to the end of the string of destination, adds a max of n characters.
 char* new_strncat(char* destination, const char* source, size_t n)
 {
     int desIndex = new_strlen(destination);
 
     int sourceIndex = 0;
-    while(source[sourceIndex] != '\0' || sourceIndex < n)
+    while(source[sourceIndex] != '\0' && sourceIndex < n)
     {
         destination[desIndex] = source[sourceIndex];
         desIndex++; 
-        sourceIndex;
+        sourceIndex++;
     }
+    destination[desIndex] = '\0';
     return destination;
 }
 
+//returns the number of characters before the null character
 size_t new_strlen(const char* string)
 {
     int index = 0;
@@ -90,3 +99,20 @@ size_t new_strlen(const char* string)
     }
     return index;
 }
+
+//returns a pointer to the first occurence of character in string or a null pointer if the character cant be found
+char* new_strchr(const char* string, int character)
+{
+
+}
+
+//returns a pointer to the first occurrence of the string contained in needle in haystack or a null pointer if it cannot be found
+char* new_strstr(const char* haystack, const char* needle)
+{
+
+}
+
+
+
+
+

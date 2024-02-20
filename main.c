086838dcfd4feb3ca6138afe5843b98dd9e58417
Zachary Stray <99822095+Heatos@users.noreply.github.com>
2024-02-20 12:48:11 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,69 +1,76 @@
 #include <stdio.h>
-#include <new_string.h> //is not working? 
+#include "new_string.h"
 #include <string.h>
 
 int main()
 {
 
-	char array[100];
-	
-	char cat[100];
-	cat = [dog];
-	 
+    char array[100];	 
 	
 	//our functions
     //new_strcpy(char* destination, const char* source)
 	//new_strncpy(char* destination, const char* source, size_t n)
-   //new_strcmp(const char* string1, const char* string2)
-   //new_strncmp(const char* string1, const char* string2, size_t n)
-   //new_strcat(char* destination, const char* source)
-   //new_strncat(char* destination, const char* source, size_t n)
-   //new_strlen(const char* string)
-   //new_strchr(const char* string, int character)
-   //new_strstr(const char* haystack, const char* needle)
+    //new_strcmp(const char* string1, const char* string2)
+    //new_strncmp(const char* string1, const char* string2, size_t n)
+    //new_strcat(char* destination, const char* source)
+    //new_strncat(char* destination, const char* source, size_t n)
+    //new_strlen(const char* string)
+    //new_strchr(const char* string, int character)
+    //new_strstr(const char* haystack, const char* needle)
    
 	new_strcpy(array, "cater");
-	printf("%c",array);
+	printf("Output: \"%s\"\n",array); 
 
 	new_strncpy(array,"doggie", 3);
-	//printf("%c",array);
+	printf("Output: \"%s\"\n",array);
 
-	new_strcmp("doh","doh");
-	//printf("%c",array);
+	int dohInt = new_strcmp("\201","A");
+    int dohInt2 = strcmp("\201", "A");
+	printf("Output: \"%d\"\n",dohInt);
+    printf("Output: \"%d\"\n",dohInt2);
 
-	new_strncmp("dar","cat",2);
-	//printf("%c",array);
+	int int1 = new_strncmp("abcdefg","abcfg", 4);
+    int int2 = strncmp("abcdefg","abcfg ", 4);
+	printf("Output: \"%d\"\n",int1);
+    printf("Output: \"%d\"\n",int2);
 
-	new_strcat(array,"");
-	//printf("%c",array);
+    char newString[100];
+	new_strcat(newString, array);
+	printf("Sample: %s\n",array);
+    printf("Output: %s\n", newString);
 
-	new_strncat();
-	//printf("%c",array);
+	new_strncat(newString, array, 2);
+	printf("Sample: %s\n",array);
+    printf("Output: %s\n", newString);
 
-	new_strlen("dog");
-	//printf("%c",array);
+	printf("Number: %lu\n", new_strlen("dog"));
    
-   //new_strchr();
-   //new_strstr();
-   
-   //library functions
+    
+    char* newPointer = new_strchr(newString, 'g');
+    printf("New Pointer: %s\n", newPointer);
 
-   strcpy();
+    newPointer = new_strstr("GeeksforGeeks", "for");
+    printf("New Pointer: %s\n", newPointer);
    
-   strncpy();
+    //library functions
+
+    char new_string[100];
+    strcpy(new_string, array);
    
-   strcmp();
+    strncpy(new_string, array, 2);
    
-   strncmp();
+    strcmp(new_string, array);
    
-   strcat();
+    strncmp(new_string, array, 1);
    
-   strncat();
+    strcat(new_string, array);
    
-   strlen("dog");
+    strncat(new_string, array, 2);
    
-   //strchar();
+    strlen("dog");
    
-   //strrchr();
+    //strchar();
+   
+    //strrchr();
     return 0;
 }
